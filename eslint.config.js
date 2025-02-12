752e3b33758dcd2f2a6b8ef30015ae5dff29d4c7
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-02-12 23:14:01 +0000

    Tune eslint & ts settings a bit

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,6 @@ import hideoo from "@hideoo/eslint-config";
 export default hideoo({
   rules: {
     "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    "@typescript-eslint/consistent-type-assertions": "off",
   },
 });
