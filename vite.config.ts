1ab267a7880d547e3d3ff9d994a50c71c7496463
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-03-07 00:23:11 +0000

    Make css module classnames containing filename in dev mode, for better DX

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { patchCssModules } from "vite-css-modules";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), tailwindcss()],
+  plugins: [tsconfigPaths(), react(), tailwindcss(), patchCssModules()],
   css: {
     transformer: "lightningcss",
+    lightningcss: {
+      cssModules: {
+        pattern:
+          process.env.NODE_ENV === "production"
+            ? "[local]_[content-hash]"
+            : "[name]_[local]_[content-hash]",
+      },
+    },
+  },
+  build: {
+    target: "es2022",
   },
 });
