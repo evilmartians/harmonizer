6de91403efc9f10e64c68cac95b2d91fb696079c
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-03-08 02:17:50 +0000

    Change `invariant` helper to take error constructor as third parameter and not the instance as a second. It will be used to create the error instance if the condition is not met.

--- src/utils/invariant.ts
+++ src/utils/invariant.ts
@@ -1,5 +1,11 @@
-export function invariant(condition: unknown, message: string): asserts condition {
+type Newable<T> = new (...args: any[]) => T;
+
+export function invariant(
+  condition: unknown,
+  message: string,
+  ErrorClass: Newable<Error> = Error,
+): asserts condition {
   if (!condition) {
-    throw new Error(message);
+    throw new ErrorClass(message);
   }
 }
