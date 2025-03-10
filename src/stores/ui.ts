4bf86ff9c6e4989ba299a5a39864442e2231e4d0
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-03-10 18:37:15 +0000

    Extend ui store for tracking scroll state in the grid

--- src/stores/ui.ts
+++ src/stores/ui.ts
@@ -1,5 +1,7 @@
 import { signal } from "@spred/core";
 
+import { createScrollStateSignal } from "@/utils/spred/createScrollStateSignal";
+
 export const $hoveredColumn = signal<null | number>(null);
 
 export function setHoveredColumn(index: number | null) {
@@ -8,4 +10,17 @@ export function setHoveredColumn(index: number | null) {
 
 export function resetHoveredColumn() {
   $hoveredColumn.set(null);
+}
+
+export const $scrollableContainer = signal<HTMLElement | null>(null);
+export const $gridHorizontalScrollState = createScrollStateSignal($scrollableContainer, "x");
+export const $gridVerticalScrollState = createScrollStateSignal($scrollableContainer, "y");
+export const $gridHorizontallyScrolled = signal(
+  (get) => get($gridHorizontalScrollState).scrollAt > 0,
+);
+
+export const $gridVerticallyScrolled = signal((get) => get($gridVerticalScrollState).scrollAt > 0);
+
+export function setScrollableContainer(container: HTMLElement | null) {
+  $scrollableContainer.set(container);
 }
