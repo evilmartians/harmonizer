a27185e01292e5f2cf49c4c3190f9d5a24de2f17
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-03-12 00:18:01 +0000

    Add base BgMode container component which defines bg-mode for the content inside

---
+++ src/components/BgMode/BgMode.tsx
@@ -0,0 +1,16 @@
+import type { ElementType } from "react";
+
+import type { BgModeType } from "./types";
+
+import type { PolymorphicComponentPropsWithRef } from "@/utils/react/polymorphic";
+
+export type BgModeProps<E extends ElementType> = PolymorphicComponentPropsWithRef<E> & {
+  className?: string;
+  bgMode: BgModeType;
+};
+
+export function BgMode<E extends ElementType>({ as, bgMode, ...restProps }: BgModeProps<E>) {
+  const Component = as ?? ("div" as ElementType);
+
+  return <Component {...restProps} data-bg-mode={bgMode} />;
+}
