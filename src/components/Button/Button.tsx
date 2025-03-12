070754a35338d7bc7ddd76bc714cbd3b4662b4a3
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-03-12 00:20:24 +0000

    Add new Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,48 @@
+import clsx from "clsx";
+import type { ComponentPropsWithRef } from "react";
+
+import styles from "./Button.module.css";
+
+import { extendableProp, type ExtendableProp } from "@/utils/react/extendableProp";
+
+export type ButtonSize = "xs" | "s" | "m" | "l";
+export type ButtonKind = "primary" | "ghost";
+export type ButtonProps = ComponentPropsWithRef<"button"> & {
+  size: ButtonSize;
+  kind?: ButtonKind;
+  rounded?: boolean;
+  iconStart?: ExtendableProp<{ className?: string }>;
+  icon?: ExtendableProp<{ className?: string }>;
+  iconEnd?: ExtendableProp<{ className?: string }>;
+};
+
+export function Button({
+  className,
+  type = "button",
+  kind = "primary",
+  size,
+  iconStart,
+  icon,
+  iconEnd,
+  rounded,
+  children,
+  ...restProps
+}: ButtonProps) {
+  return (
+    <button
+      type={type}
+      className={clsx(
+        styles.button,
+        styles[`kind_${kind}`],
+        styles[`size_${size}`],
+        rounded && styles.rounded,
+        className,
+      )}
+      {...restProps}
+    >
+      {extendableProp(iconStart, { className: styles.icon })}
+      {icon ? extendableProp(icon, { className: styles.icon }) : children}
+      {extendableProp(iconEnd, { className: styles.icon })}
+    </button>
+  );
+}
