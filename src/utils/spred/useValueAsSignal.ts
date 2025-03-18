d9b385e73fa6baec1a3a840ef07b905e0956f07c
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-03-18 10:32:56 +0000

    Add spred util react hook: it allows creating signal based on the value, and updates signal value when value is changed

---
+++ src/utils/spred/useValueAsSignal.ts
@@ -0,0 +1,26 @@
+import { signal } from "@spred/core";
+import { useEffect, useRef } from "react";
+
+/**
+ * This is the util for creating signal from value in react components / hooks.
+ * This allows using values in useSignal hook and track its change.
+ *
+ * @param value
+ * @returns
+ */
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+export function useValueAsSignal<T>(value: Exclude<T, Function>) {
+  const isFirstRender = useRef(true);
+  const ref = useRef(signal(value));
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+
+    ref.current.set(value);
+  }, [value]);
+
+  return ref.current;
+}
