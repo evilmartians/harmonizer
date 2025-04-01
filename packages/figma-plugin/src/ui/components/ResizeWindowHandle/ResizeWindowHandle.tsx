4cfc000d41b103812be70f56d41d0df821629c39
Anton Lovchikov <antiflasher@gmail.com>
2025-04-01 14:18:57 +0100

    Change the resize handle

--- packages/figma-plugin/src/ui/components/ResizeWindowHandle/ResizeWindowHandle.tsx
+++ packages/figma-plugin/src/ui/components/ResizeWindowHandle/ResizeWindowHandle.tsx
@@ -42,10 +42,18 @@ export function ResizeWindowHandle({ onResize }: ResizeWindowHandleProps) {
       onPointerDown={handlePointerDown}
       onPointerUp={handlePointerUp}
     >
-      <svg viewBox="0 0 16 16">
+      <svg
+        width="12"
+        height="12"
+        viewBox="0 0 12 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
         <path
-          fill="currentColor"
-          d="M4.226 16H1L16 1v3.226L4.226 16ZM11.226 16H8l8-8v3.226L11.226 16Z"
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M11 0C11.5523 0 12 0.447715 12 1C12 7.07513 7.07513 12 1 12C0.447715 12 0 11.5523 0 11C0 10.4477 0.447715 10 1 10C5.97056 10 10 5.97056 10 1C10 0.447715 10.4477 0 11 0Z"
+          fill="#808080"
         />
       </svg>
     </button>
