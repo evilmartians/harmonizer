2ec63088f6d85613a8d099d6494ce2b1a10c58ab
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-04-04 22:38:57 +0100

    Refactor worker channels to use new static createChannel method

--- packages/core/src/worker/worker.ts
+++ packages/core/src/worker/worker.ts
@@ -1,13 +1,9 @@
 import { calculateColors } from "@core/utils/colors/calculateColors";
-import { CommunicationChannel } from "@core/utils/communication-channel/CommunicationChannel";
 import { PostMessageTransport } from "@core/utils/communication-channel/transports/PostMessageTransport";
 
 import type { ClientMessages, WorkerMessages } from "./types";
 
-const postMessageTransport = new PostMessageTransport<ClientMessages, WorkerMessages>();
-const clientChannel = new CommunicationChannel<ClientMessages, WorkerMessages>(
-  postMessageTransport,
-);
+const clientChannel = PostMessageTransport.createChannel<ClientMessages, WorkerMessages>();
 
 clientChannel.on("generate:colors", function (payload) {
   calculateColors(payload, (payload) => {
