0ac27e98f00363ccbcd94a4dc3da3e6618b287c4
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-04-09 15:45:30 +0100

    Make our Select element completely custom: reuse list styles for the dropdown

--- packages/core/src/components/Select/Select.tsx
+++ packages/core/src/components/Select/Select.tsx
@@ -1,44 +1,82 @@
 import { Button, type ButtonKind, type ButtonSize } from "@core/components/Button/Button";
+import { MCheck } from "@core/components/Icon/MCheck";
 import { MTriangleDown } from "@core/components/Icon/MTriangleDown";
+import { List } from "@core/components/List/List";
+import { ListItem } from "@core/components/List/ListItem";
+import { mergeProps } from "@core/utils/react/mergeProps";
+import { normalizeProps, Portal, useMachine } from "@zag-js/react";
+import * as select from "@zag-js/select";
 import clsx from "clsx";
-import { useRef, type ComponentPropsWithRef } from "react";
+import { type ComponentProps, useId } from "react";
 
 import styles from "./Select.module.css";
 
-export type SelectProps = Omit<ComponentPropsWithRef<"select">, "size"> & {
+export type SelectProps = Omit<ComponentProps<"select">, "size" | "value" | "multiple"> & {
   size: ButtonSize;
   kind?: ButtonKind;
-  value: string;
   placeholder?: string;
   options: { value: string; label: string }[];
-};
+} & ({ multiple: true; value: string[] } | { multiple?: false; value: string });
 
 export function Select({
   className,
   kind = "primary",
   size,
   value,
+  multiple,
   options,
   placeholder = "Select an option",
-  ...restProps
+  onChange,
 }: SelectProps) {
-  const selectRef = useRef<HTMLSelectElement | null>(null);
-  const selected = options.find((opt) => opt.value === value);
+  const collection = select.collection({
+    items: options,
+    itemToString: (item) => item.label,
+    itemToValue: (item) => item.value,
+  });
+  const service = useMachine(select.machine, {
+    id: useId(),
+    collection,
+    multiple,
+    defaultValue: multiple ? value : [value],
+    positioning: { offset: { mainAxis: 2, crossAxis: 0 } },
+  });
+  const api = select.connect(service, normalizeProps);
 
   return (
-    <div className={clsx(styles.container, className)}>
-      <select ref={selectRef} className={styles.nativeSelect} value={value} {...restProps}>
-        {options.map((opt) => (
-          <option key={opt.value} value={opt.value}>
-            {opt.label}
+    <div {...mergeProps(api.getRootProps(), { className: clsx(styles.container, className) })}>
+      <select {...mergeProps(api.getHiddenSelectProps(), { onChange })}>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
           </option>
         ))}
       </select>
+      <div {...api.getControlProps()}>
+        <Button {...mergeProps(api.getTriggerProps(), { size, kind, className: styles.button })}>
+          {api.valueAsString || placeholder}
+          <MTriangleDown className={clsx(styles.icon, styles[`size_${size}`])} />
+        </Button>
+      </div>
 
-      <Button tabIndex={-1} size={size} kind={kind} className={styles.button}>
-        {selected?.label ?? placeholder}
-        <MTriangleDown className={clsx(styles.icon, styles[`size_${size}`])} />
-      </Button>
+      <Portal>
+        <div {...api.getPositionerProps()}>
+          <List {...mergeProps(api.getContentProps(), { className: styles.dropdown })}>
+            {options.map((item) => (
+              <ListItem
+                {...mergeProps(api.getItemProps({ item }), {
+                  className: styles.item,
+                })}
+                key={item.value}
+              >
+                {item.label}
+                <span {...api.getItemIndicatorProps({ item })}>
+                  <MCheck className={styles.checkIcon} />
+                </span>
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </Portal>
     </div>
   );
 }
