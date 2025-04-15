76daadb8025dca01f52c8c7919db6511c4d06af6
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-04-15 21:12:06 +0100

    Describe a bit the logic behind components generating dynamic grid styles

--- packages/core/src/components/Grid/GridStylesHueHover.tsx
+++ packages/core/src/components/Grid/GridStylesHueHover.tsx
@@ -9,6 +9,12 @@ import {
   REMOVE_BUTTON_HOVER_STYLE,
 } from "./constants";
 
+/**
+  This function generates CSS selectors and styles for the remove hue button when the hue row is hovered, focused, or has a focus-visible element.
+  The row is determined by the DATA_ATTR_CELL_HUE_ID atribute and hueId.
+  Such approach allows making the button visible without any JS logic.
+  The selectors are generated for each hue row in the grid.
+*/
 export const GridStylesHueHover = memo(function GridStylesHueHover() {
   const hueIds = useSubscribe($hueIds);
   const selectors = hueIds.flatMap((hueId) => {
