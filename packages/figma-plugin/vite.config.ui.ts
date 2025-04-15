76ff2cf7e9eef75fb817167e17abd5a7c78c44a8
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-04-15 11:57:27 +0100

    Setup browserslist for targets

--- packages/figma-plugin/vite.config.ui.ts
+++ packages/figma-plugin/vite.config.ui.ts
@@ -2,16 +2,34 @@ import path from "node:path";
 
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import browserslist from "browserslist";
+import browserslistToEsbuild from "browserslist-to-esbuild";
+import { Features, browserslistToTargets } from "lightningcss";
 import { defineConfig } from "vite";
 import { patchCssModules } from "vite-css-modules";
 import { viteSingleFile } from "vite-plugin-singlefile";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const browserslistConfig = browserslist(undefined, { config: "./.browserslistrc" });
+
 export default defineConfig(({ mode }) => ({
   plugins: [tsconfigPaths(), react(), tailwindcss(), patchCssModules(), viteSingleFile()],
   root: "./src/ui",
+  css: {
+    transformer: "lightningcss",
+    lightningcss: {
+      targets: browserslistToTargets(browserslistConfig),
+      exclude: Features.DirSelector,
+      cssModules: {
+        pattern:
+          process.env.NODE_ENV === "production"
+            ? "[local]_[content-hash]"
+            : "[name]_[local]_[content-hash]",
+      },
+    },
+  },
   build: {
-    target: "esnext",
+    target: browserslistToEsbuild(browserslistConfig),
     minify: mode === "production",
     cssMinify: mode === "production",
     sourcemap: mode === "production" ? false : "inline",
