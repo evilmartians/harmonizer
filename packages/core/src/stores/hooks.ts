97370ec853b2a7dc43924a3028be63ac2ca94891
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-04-17 01:47:52 +0100

    Get rid of derived atom in the useLevelBgMode, because it led to the incorrect state during removing levels or updating the whole config

--- packages/core/src/stores/hooks.ts
+++ packages/core/src/stores/hooks.ts
@@ -1,4 +1,4 @@
-import { useSignal, useSubscribe } from "@spred/react";
+import { useSubscribe } from "@spred/react";
 
 import type { BgModeType } from "@core/components/BgMode/types";
 import { $levelIdsToIndex } from "@core/stores/colors";
@@ -8,13 +8,13 @@ import { invariant } from "@core/utils/assertions/invariant";
 import { $bgColorDarkBgMode, $bgColorLightBgMode, $bgLightStart } from "./settings";
 
 export function useLevelBgMode(levelId: LevelId): BgModeType {
-  const $levelBgMode = useSignal((get) => {
-    const levelIndex = get($levelIdsToIndex)[levelId];
+  const levelIdsToIndex = useSubscribe($levelIdsToIndex);
+  const bgLightStart = useSubscribe($bgLightStart);
+  const bgColorDarkBgMode = useSubscribe($bgColorDarkBgMode);
+  const bgColorLightBgMode = useSubscribe($bgColorLightBgMode);
+  const levelIndex = levelIdsToIndex[levelId];
 
-    invariant(levelIndex !== undefined, "Level not found for the given ID");
+  invariant(levelIndex !== undefined, "Level not found for the given ID");
 
-    return levelIndex < get($bgLightStart) ? get($bgColorDarkBgMode) : get($bgColorLightBgMode);
-  });
-
-  return useSubscribe($levelBgMode);
+  return levelIndex < bgLightStart ? bgColorDarkBgMode : bgColorLightBgMode;
 }
