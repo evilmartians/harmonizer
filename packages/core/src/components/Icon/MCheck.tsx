c785260f34def317e9143ca9358b045e39ff8cde
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-05-05 11:40:20 +0100

    Set fill=none for the stroke svg icons, to fix wrong fill

--- packages/core/src/components/Icon/MCheck.tsx
+++ packages/core/src/components/Icon/MCheck.tsx
@@ -5,7 +5,7 @@ export type MCheckProps = SVGProps<SVGSVGElement>;
 export function MCheck(props: MCheckProps) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
-      <path stroke="currentColor" d="M3 8.385 7.286 13 13 3" />
+      <path stroke="currentColor" fill="none" d="M3 8.385 7.286 13 13 3" />
     </svg>
   );
 }
--- packages/core/src/components/Icon/MCross.tsx
+++ packages/core/src/components/Icon/MCross.tsx
@@ -7,6 +7,7 @@ export function MCross(props: MCrossProps) {
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
       <path
         stroke="currentColor"
+        fill="none"
         d="M3.757 3.757 8 8m0 0 4.243 4.243M8 8l-4.243 4.243M8 8l4.243-4.243"
       />
     </svg>
